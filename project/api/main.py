601fbb4e30a8941910f2d130f8af7a32e67e11de
Ryan Butler <butryan@amazon.com>
2021-05-05 13:49:35 +0200

    Completed Postgres part

--- project/api/main.py
+++ project/api/main.py
@@ -1,18 +1,25 @@
-import uvicorn
+import os
+
 from api.config import Settings, get_settings
 from fastapi import Depends, FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 api = FastAPI()
 
 
-@api.get("/")
+register_tortoise(
+    api,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["api.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
+
+
+@api.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
     return {
         "Ping": "Pong",
         "environment": settings.environment,
         "testing": settings.testing,
     }
-
-
-if __name__ == "__main__":
-    uvicorn.run(api, port=8000, host="127.0.0.1", reload=True)
